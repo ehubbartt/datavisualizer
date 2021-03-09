5f44e1296d9945de5e8930f532ed528771da50fc
Ethan Hubbartt <ehubb@uw.edu>
2021-03-08 16:20:53 -0800

    made nav bar better and added scrolling for home

---
+++ home.js
@@ -0,0 +1,28 @@
+(function() {
+  const sections = [...document.querySelectorAll("section")];
+
+  let options = {
+    rootMargin: "0px",
+    threshold: 0.25
+  };
+
+  const callback = (entries, observer) => {
+    console.log('hello');
+    entries.forEach(entry => {
+      const { target } = entry;
+      console.log(entry, target)
+
+      if (entry.intersectionRatio >= 0.25) {
+        target.classList.add("is-visible");
+      } else {
+        target.classList.remove("is-visible");
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  sections.forEach((section, index) => {
+    observer.observe(section);
+  });
+})();
